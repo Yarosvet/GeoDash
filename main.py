14b8f98b43d7c9c4ad07964a161714720745c6ac
Yarosvet <000xsz839>
2020-01-19 21:38:45 +0300

    Version 1.0 - corrections

--- main.py
+++ main.py
@@ -2,71 +2,8 @@
 import sys
 import pygame
 import os
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sprite_group, sheet, columns, rows, x, y):
-        super().__init__(sprite_group)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-
-
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, group, image):
-        super().__init__(group)
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
-
-    def update(self, x, y):
-        self.rect.x, self.rect.y = (x, y)
-
-
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2 - 120)
-
+# local
+from Classes import *
 
 # Переменные и константы
 size = width, height = 800, 500
@@ -89,7 +26,7 @@ platforms = pygame.sprite.Group()
 DieBlocks = pygame.sprite.Group()
 characters = pygame.sprite.Group()
 character = None
-cam = Camera()
+cam = Camera(width, height)
 
 # Инициализация игры
 screen = pygame.display.set_mode(size)
@@ -188,7 +125,7 @@ while True:
                         DieBlocks = pygame.sprite.Group()
                         characters = pygame.sprite.Group()
                         character = None
-                        cam = Camera()
+                        cam = Camera(width, height)
                     len_level = build_level('data/level_1.txt')
                     # Музыка на уровне
                     pygame.mixer.music.stop()
